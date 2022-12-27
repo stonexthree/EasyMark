8ee5a86307422895fa79fafe857a91d36b342ad9
stonexthree <stonexthree@outlook.com>
2022-12-28 00:30:06 +0800

    初步调整页面样式及主题

---
+++ page/myMarkDoc/src/components/sub/LoginBackground.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="component-root">
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "CircleBackground"
+}
+</script>
+<script setup lang="ts">
+import {ref,Ref,onMounted} from 'vue'
+
+const canvas= ref<HTMLCanvasElement|null>(null);
+
+</script>
+
+<style scoped>
+canvas{
+  width: 100%;
+  height: 100%;
+}
+</style>
